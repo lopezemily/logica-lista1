5ae692e7dbfb2ecad772124e76b0186471696ba5
Lopez <emilylopez@deloitte.com>
2018-05-06 20:37:30 -0300

    problema 2 resolvido

---
+++ problemas/exercicio02/media parcial/src/media/parcial/MediaParcial.java
@@ -0,0 +1,34 @@
+package media.parcial;
+
+import java.util.Scanner;
+
+public class MediaParcial {
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        float nota1, nota2, notaparcial;
+        Scanner status;
+
+        System.out.println("Qual foi sua nota da N1?");
+        nota1 = leia.nextFloat();
+
+        System.out.println("Qual foi sua nota da N2?");
+        nota2 = leia.nextFloat();
+
+        notaparcial = (nota1 + nota2) / 2;
+        System.out.println("Sua nota parcial é: " + notaparcial);
+
+        if (notaparcial >= 10) {
+            System.out.println("PARABÉNS! Você foi aprovado com honra! :D");
+        } else if (notaparcial >= 7) {
+            System.out.println("Muito bem! Você está aprovado! :)");
+        } else if (notaparcial >= 5) {
+            System.out.println("Cuidado! Você fidcou de exame...");
+        } else {
+            System.out.println("Que pena! Você foi reprovado! :(");
+        }
+    }
+    
+}
