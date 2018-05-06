8ef994aad093b378fdfdb40e5a93b754ad360fb4
Lopez <emilylopez@deloitte.com>
2018-05-06 20:01:01 -0300

    problema 1 resolvido

--- problemas/exercicio01/media/src/media/Media.java
+++ problemas/exercicio01/media/src/media/Media.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package media;
 
-/**
- *
- * @author emilylopez
- */
+import java.util.Scanner;
+
 public class Media {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner leia = new Scanner(System.in);
+
+        float nota1, nota2, mediafinal;
+        String status;
+        
+        System.out.println("Qual foi sua nota 1?");
+        nota1 = leia.nextFloat();
+        
+        System.out.println("Qual foi a sua nota 2?");
+        nota2 = leia.nextFloat();
+        
+        mediafinal = (nota1 + nota2) / 2;
+        System.out.println("Sua média final é " + mediafinal);
+        
+        if (mediafinal > 5){
+            System.out.println("Parabéns! Você foi aprovado!");            
+        } else{
+            System.out.println("Que pena! Você foi reprovado!");
+        }
     }
-    
 }
